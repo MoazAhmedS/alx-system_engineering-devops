8d4df783ad1908bf119644d8ba2caae62d34ca7a
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2024-01-26 15:18:08 +0000

    t

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers"""
+    """
+    A function that queries the Reddit API and returns
+    the number of subscribers
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/guest)"}
+
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json()
+        total_subscribers = data['data']['subscribers']
+        return total_subscribers
+    else:
         return 0
-    results = response.json().get("data")
-     return results.get("subscribers")
